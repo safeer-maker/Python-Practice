3cbedd4311cae8dfd83984ab8040987b2553b3fa
Safeer <bsee15055@itu.edu.pk>
2020-04-15 12:13:22 +0500

    Odd and Even Completed
    All the requirments of the task completed
    https://www.practicepython.org/exercise/2014/02/05/02-odd-or-even.html

--- Praticepython.org/Odd or Even task 2.py
+++ Praticepython.org/Odd or Even task 2.py
@@ -1,7 +1,18 @@
 
+
 def odd(data = 0, divider = 2):
-    print("sam")
-    print(data)
+    mode = int(data) % int(divider)
+    if (int(divider) ==2):
+        if (mode == 0):
+            print("The number " + str(data) + " is an EVEN number")
+        else:
+            print("The number " + str(data) + " is an ODD number")
+    else:
+        if (mode == 0):
+            print("The number " + str(data) + " is diviser of " + str(divider))
+        else:
+            print("The number " + str(data) + " is NOT diviser of " + str(divider))
+
 
 
 def inpt():
@@ -14,6 +25,5 @@ def inpt():
 inp = list(inpt())
 print(inp)
 odd(data=inp[0])
-
-
+odd(inp[0], inp[1])
 
