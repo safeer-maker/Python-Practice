1da65a3b01c4e0aa5a54c93ec08958892d1b92d2
Safeer <bsee15055@itu.edu.pk>
2020-04-16 08:01:21 +0500

    List Less Than Ten completed
    task 3 completed from web
    https://www.practicepython.org/exercise/2014/02/15/03-list-less-than-ten.html

--- Praticepython.org/List Less Than Ten task3.py
+++ Praticepython.org/List Less Than Ten task3.py
@@ -1,6 +1,10 @@
 
-def less_then():
-    print ("dumy")
+def less_then(a, const = 5):
+    less_list = []
+    for i in range(len(a)):
+        if int(a[i]) < const:
+            less_list.append(a[i])
+    return less_list
 
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
@@ -9,3 +13,6 @@ for i in range(len(a)):
     if int(a[i]) < 5:
         print (a[i])
 
+print(less_then(a))
+const = int(input("please input the number: "))
+print(less_then(a,const))
