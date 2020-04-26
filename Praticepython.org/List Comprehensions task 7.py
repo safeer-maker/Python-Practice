d338d9ae8c11b99833e8d94fab8ee7f5e5ada7b8
Safeer Ahmad <bsee15055@itu.edu.pk>
2020-04-27 01:07:51 +0500

    List Comprehensions task 7 uncomplete
    just testing the list and doing the experiments

---
+++ Praticepython.org/List Comprehensions task 7.py
@@ -0,0 +1,14 @@
+'''
+years_of_birth = [1990, 1991, 1990, 1990, 1992, 1991]
+ages = []
+for year in years_of_birth:
+    ages.append(2014 - year)
+
+print(ages)
+'''
+
+ye = [1, 5, 10, 15, 20, 25, 30]
+ages = [5 * a for a in ye]
+
+print(ages)
+
